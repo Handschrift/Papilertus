02139f04bf33942b4f76b0a3f8c460ab50a4ebc1
handschrift <cahit.kilinc@gmx.de>
2021-10-28 23:46:10 +0200

    Added Config logic

--- src/main/java/com/openfunbot/config/Config.java
+++ src/main/java/com/openfunbot/config/Config.java
@@ -1,4 +1,55 @@
 package com.openfunbot.config;
 
-public class Config {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+
+public final class Config {
+
+    private static transient Config config = new Config();
+    private transient final File file = new File("config.json");
+
+    private String token = "";
+
+    public static Config getConfig() {
+        config = config.read();
+        return config;
+    }
+
+    private void save() {
+        final Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+        try (FileWriter writer = new FileWriter(file)) {
+            gson.toJson(this, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public Config read() {
+        final Gson gson = new Gson();
+        try (FileReader reader = new FileReader(file)) {
+            final BufferedReader bufferedReader = new BufferedReader(reader);
+            return gson.fromJson(bufferedReader, Config.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Config() {
+        if (!file.exists()) {
+            save();
+        }
+    }
+
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
--- src/main/java/com/openfunbot/init/OpenFunBot.java
+++ src/main/java/com/openfunbot/init/OpenFunBot.java
@@ -1,7 +1,10 @@
 package com.openfunbot.init;
 
+import com.openfunbot.config.Config;
+
 public class OpenFunBot {
     public static void main(String[] args) {
-
+        Config config = Config.getConfig();
+        System.out.println(config.getToken());
     }
 }
