e6ceceac5870053e3a11f47f72291c8553c3a55a
handschrift <cahit.kilinc@gmx.de>
2021-11-17 19:42:22 +0100

    Added Module

---
+++ BestFriendLegacyPlugin/src/main/java/com/github/bestfriendplugin/BestFriend.java
@@ -0,0 +1,34 @@
+package com.github.bestfriendplugin;
+
+import com.github.bestfriendplugin.commands.*;
+import com.openpackagedbot.commands.core.Command;
+import com.openpackagedbot.plugin.Plugin;
+import com.openpackagedbot.plugin.PluginData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BestFriend implements Plugin {
+
+
+    @Override
+    public void onLoad(PluginData pluginData) {
+
+    }
+
+    @Override
+    public List<Command> getCommands() {
+        ArrayList<Command> commands = new ArrayList<>();
+        commands.add(new ZodiacCommand());
+        commands.add(new PollCommand());
+        commands.add(new LuckCommand());
+        commands.add(new UnluckCommand());
+        commands.add(new PickupCommand());
+        return commands;
+    }
+
+    @Override
+    public void onUnload() {
+
+    }
+}
