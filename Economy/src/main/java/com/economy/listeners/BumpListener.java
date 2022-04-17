83418b1889fe8eeefd1ffeec5dadaff1b43dda03
Handschrift <cahit.kilinc@gmx.de>
2022-04-17 21:32:40 +0200

    Added bump listener

---
+++ Economy/src/main/java/com/economy/listeners/BumpListener.java
@@ -0,0 +1,13 @@
+package com.economy.listeners;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class BumpListener extends ListenerAdapter {
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+
+    }
+}
