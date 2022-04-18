330805d8f5a870b0179f2f7520162701d49c99ab
Handschrift <cahit.kilinc@gmx.de>
2022-04-18 14:28:40 +0200

    Added Waiter and new upgrades to config

--- Economy/src/main/java/com/economy/init/Economy.java
+++ Economy/src/main/java/com/economy/init/Economy.java
@@ -1,8 +1,13 @@
 package com.economy.init;
 
 import com.economy.commands.ProfileCommand;
+import com.economy.commands.ShopCommand;
+import com.economy.commands.WorkCommand;
+import com.economy.game.element.GameUpgrade;
+import com.economy.game.element.IncrementType;
 import com.economy.listeners.MessageReceivedListener;
 import com.economy.listeners.VoiceJoinListener;
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import com.openpackagedbot.commands.core.Command;
 import com.openpackagedbot.plugin.Plugin;
 import com.openpackagedbot.plugin.PluginConfig;
@@ -17,13 +22,17 @@ public class Economy implements Plugin {
     //singleton for database
     private static PluginDataStore store;
     private static PluginConfig config;
+    private final EventWaiter waiter = new EventWaiter();
 
     @Override
     public void onLoad(PluginData pluginData) {
         store = new PluginDataStore(pluginData);
         config = new PluginConfig(pluginData);
-        config.addEntry("currency_name", "Butterflies");
-        config.addEntry("currency_icon", "<:papilertus:963118768614150224>");
+        //<:papilertus:963118768614150224>
+        config.addEntry("currency_name", "Plants");
+        config.addEntry("currency_icon", ":deciduous_tree:");
+        config.addEntry("collectable_name", "Seeds");
+        config.addEntry("collectable_icon", ":seedling:");
         config.addEntry("coin_on_message_sent", true);
         config.addEntry("coin_message_cooldown", 20);
         config.addEntry("coin_on_voice_activity", true);
@@ -33,12 +42,26 @@ public class Economy implements Plugin {
         config.addEntry("whitelist_mode", false);
         config.addEntry("listed_users", new String[]{});
         config.addEntry("listed_roles", new String[]{});
+        config.addEntry("enable_work_minigame", true);
+        config.addEntry("work_cooldown", 60);
+        config.addEntry("base_work_gain", 20);
+        config.addEntry("convert_command_name", "plant");
+        config.addEntry("upgrades", new GameUpgrade[]{
+                new GameUpgrade("Monarch Butterfly", "Upgrades seed gain by voice", IncrementType.VOICE, ":butterfly:", 1.2F, 1.1F),
+                new GameUpgrade("Periander Metalmark", "Upgrades seed gain by message", IncrementType.MESSAGE, ":butterfly:", 1.2F, 1.1F),
+                new GameUpgrade("Mountain Apollo", "Upgrades seed gain by work", IncrementType.WORK, ":butterfly:", 1.2F, 1.1F),
+                new GameUpgrade("Great Purple Hairsteak", "Upgrades seed gain by bump", IncrementType.BUMP, ":butterfly:", 1.2F, 1.1F),
+                new GameUpgrade("Southern Dogface", "Upgrades seed gain by treasure", IncrementType.TREASURE, ":butterfly:", 1.2F, 1.1F)
+        });
     }
 
     @Override
     public List<Command> getCommands() {
+
         final ArrayList<Command> commands = new ArrayList<>();
         commands.add(new ProfileCommand());
+        commands.add(new WorkCommand(waiter));
+        commands.add(new ShopCommand());
         return commands;
     }
 
@@ -47,6 +70,7 @@ public class Economy implements Plugin {
         final ArrayList<EventListener> listeners = new ArrayList<>();
         listeners.add(new MessageReceivedListener());
         listeners.add(new VoiceJoinListener());
+        listeners.add(waiter);
         return listeners;
     }
 
