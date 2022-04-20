2208b1607361715aed9457282283a13d3ccf50aa
Handschrift <cahit.kilinc@gmx.de>
2022-04-20 17:20:43 +0200

    Added null check

--- src/main/java/com/openpackagedbot/gui/button/ButtonListener.java
+++ src/main/java/com/openpackagedbot/gui/button/ButtonListener.java
@@ -9,6 +9,10 @@ public class ButtonListener extends ListenerAdapter {
     @Override
     public void onButtonClick(@NotNull ButtonClickEvent event) {
         final DiscordButton button = ButtonRegistry.getButtonById(event.getComponentId());
+        if (button == null) {
+            event.reply("This button expired! Please execute the command again.").setEphemeral(true).queue();
+            return;
+        }
         if (!button.getId().isEmpty() && !event.getUser().getId().equals(button.getUserId())) {
             event.reply("You are not authorized to press this button").setEphemeral(true).queue();
             return;
