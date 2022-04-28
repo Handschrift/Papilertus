e3a29dff1e0f0344a4a403c7921b07cdcf9b26c7
Handschrift <cahit.kilinc@gmx.de>
2022-04-28 15:33:47 +0200

    added inventory classes

---
+++ Economy/src/main/java/com/economy/database/models/EconomyUserInventoryEntry.java
@@ -0,0 +1,49 @@
+package com.economy.database.models;
+
+import com.economy.init.Economy;
+
+import java.util.concurrent.TimeUnit;
+
+public class EconomyUserInventoryEntry {
+    private final float count;
+    private final long timeAdded;
+
+    public EconomyUserInventoryEntry(float count, long timeAdded) {
+        this.count = count;
+        this.timeAdded = timeAdded;
+    }
+
+    public String getName() {
+        if(isGrowing()){
+            return Economy.getConfig().readString("collectable_name");
+        }
+        if(isMature()){
+            return Economy.getConfig().readString("currency_name");
+        }
+        if(isDead()){
+            return "Dead";
+        }
+        return "UNKNOWN";
+    }
+
+    public float getCount() {
+        return count;
+    }
+
+    public long getTimeAdded() {
+        return timeAdded;
+    }
+
+    public boolean isGrowing() {
+        return System.currentTimeMillis() - timeAdded < TimeUnit.HOURS.toMillis(1);
+    }
+
+    public boolean isMature() {
+        return System.currentTimeMillis() - timeAdded > TimeUnit.HOURS.toMillis(1)
+                && System.currentTimeMillis() - timeAdded < TimeUnit.HOURS.toMillis(3);
+    }
+
+    public boolean isDead() {
+        return System.currentTimeMillis() - timeAdded > TimeUnit.HOURS.toMillis(3);
+    }
+}
