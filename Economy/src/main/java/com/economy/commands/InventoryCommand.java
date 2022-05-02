e520ccc5b92e9be8136bc4dd9ff69b05b0d1568d
Handschrift <cahit.kilinc@gmx.de>
2022-05-02 09:22:36 +0200

    added time until grown indicator

--- Economy/src/main/java/com/economy/commands/InventoryCommand.java
+++ Economy/src/main/java/com/economy/commands/InventoryCommand.java
@@ -4,6 +4,7 @@ import com.economy.database.databases.UserDatabase;
 import com.economy.database.models.EconomyUser;
 import com.economy.database.models.EconomyUserInventory;
 import com.economy.database.models.EconomyUserInventoryEntry;
+import com.economy.init.Economy;
 import com.openpackagedbot.commands.core.Command;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -11,6 +12,7 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
 import java.time.Instant;
 import java.time.ZoneId;
+import java.util.concurrent.TimeUnit;
 
 public class InventoryCommand extends Command {
 
@@ -32,10 +34,12 @@ public class InventoryCommand extends Command {
         }
 
         for (EconomyUserInventoryEntry entry : inventory.getEntries()) {
-            builder.addField(entry.getName() + " (" + entry.getCount() + ")", "Added: " + Instant.ofEpochMilli(entry.getTimeAdded()).atZone(ZoneId.systemDefault()).toLocalDate(), false);
+            final long future = TimeUnit.MILLISECONDS.toSeconds(TimeUnit.DAYS.toMillis(1) + entry.getTimeAdded());
+            builder.addField(entry.getName() + " (" + entry.getCount() + ")", "Added: " + Instant.ofEpochMilli(entry.getTimeAdded()).atZone(ZoneId.systemDefault()).toLocalDate() + "\n"
+                    + "Will grow: <t:" + future + ":R>", false);
         }
 
-        builder.setFooter("You need to wait for 3 days for the seeds to grow. After 8 days the plants will rot.");
+        builder.setFooter("You need to wait for 1 day for the seeds to grow. After 3 days the plants will rot.");
 
         slashCommandInteractionEvent.replyEmbeds(builder.build()).queue();
     }
