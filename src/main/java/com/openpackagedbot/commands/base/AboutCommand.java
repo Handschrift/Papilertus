a81e885a9d09c8c749c39508da83bc547f277555
Handschrift <cahit.kilinc@gmx.de>
2022-05-02 09:18:29 +0200

    added empty about command

---
+++ src/main/java/com/openpackagedbot/commands/base/AboutCommand.java
@@ -0,0 +1,11 @@
+package com.openpackagedbot.commands.base;
+
+import com.openpackagedbot.commands.core.Command;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class AboutCommand extends Command {
+    @Override
+    protected void execute(SlashCommandInteractionEvent event) {
+
+    }
+}
