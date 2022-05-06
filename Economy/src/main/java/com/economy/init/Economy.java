69deb9c19952874b190a19e6591ee44511a86233
handschrift <cahit.kilinc@gmx.de>
2022-05-06 19:17:47 +0200

    config additions and background tasks

--- Economy/src/main/java/com/economy/init/Economy.java
+++ Economy/src/main/java/com/economy/init/Economy.java
@@ -5,8 +5,6 @@ import com.economy.database.databases.UserDatabase;
 import com.economy.database.models.EconomyUser;
 import com.economy.database.models.EconomyUserInventoryEntry;
 import com.economy.game.element.Forecast;
-import com.economy.game.element.GameUpgrade;
-import com.economy.game.element.IncrementType;
 import com.economy.listeners.BumpListener;
 import com.economy.listeners.MessageReceivedListener;
 import com.economy.listeners.VoiceJoinListener;
@@ -44,39 +42,10 @@ public class Economy implements Plugin {
         config = new PluginConfig(pluginData);
         //<:papilertus:963118768614150224>
 
-      /*  config.addEntry("currency_name", "Plants");
-        config.addEntry("currency_icon", ":deciduous_tree:");
-        config.addEntry("collectable_name", "Seeds");
-        config.addEntry("collectable_icon", ":seedling:");
-        config.addEntry("coin_on_message_sent", true);
-        config.addEntry("coin_message_cooldown", 20);
-        config.addEntry("coin_on_voice_activity", true);
-        config.addEntry("base_coin_on_message_amount", 1);
-        config.addEntry("base_coin_on_voice_activity_amount", 1);
-        config.addEntry("whitelist_mode", false);
-        config.addEntry("listed_users", new String[]{});
-        config.addEntry("listed_roles", new String[]{});
-        config.addEntry("enable_work_minigame", true);
-        config.addEntry("work_cooldown", 60);
-        config.addEntry("base_work_gain", 3);
-        config.addEntry("convert_command_name", "plant");
-        config.addEntry("base_collectables_on_bump_gain", 110);
-        config.addEntry("collectable_to_currency_conversion", 2);
-        config.addEntry("decimals", 0);
-        config.addEntry("base_daily_gain", 50);
-        config.addEntry("upgrades", new GameUpgrade[]{
-                new GameUpgrade("Monarch Butterfly", "Upgrades seed gain by voice", IncrementType.VOICE, ":butterfly:", 1.1F, 36.0F),
-                new GameUpgrade("Periander Metalmark", "Upgrades seed gain by message", IncrementType.MESSAGE, ":butterfly:", 1.1F, 18.2F),
-                new GameUpgrade("Mountain Apollo", "Upgrades seed gain by work", IncrementType.WORK, ":butterfly:", 1.1F, 51.3F),
-                new GameUpgrade("Great Purple Hairstreak", "Upgrades seed gain by bump", IncrementType.BUMP, ":butterfly:", 1.8F, 100.4F),
-                new GameUpgrade("Southern Dogface", "Upgrades seed gain by treasure", IncrementType.TREASURE, ":butterfly:", 1.5F, 30.2F),
-                new GameUpgrade("Essex Skipper", "Upgrades seed gain per daily", IncrementType.DAILY, ":butterfly:", 1.1f, 35.0f)
-        });*/
-
         config.fromObject(new EconomyConfig());
         economyConfig = config.toObject(EconomyConfig.class);
 
-        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        final ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
         service.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
@@ -91,32 +60,34 @@ public class Economy implements Plugin {
         }, 24, 24, TimeUnit.HOURS);
 
         //This code is quite messy because of the performance
-        service.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                final FindIterable<Document> results = store.getIterableList(Filters.empty());
+        if (economyConfig.isAutoCollect()) {
+            service.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    final FindIterable<Document> results = store.getIterableList(Filters.empty());
 
-                for (Document current : results) {
+                    for (Document current : results) {
 
-                    final EconomyUser user = new Gson().fromJson(current.toJson(), EconomyUser.class);
-                    boolean modified = false;
-                    final ListIterator<EconomyUserInventoryEntry> iterator = user.getInventory().getEntries().listIterator();
+                        final EconomyUser user = new Gson().fromJson(current.toJson(), EconomyUser.class);
+                        boolean modified = false;
+                        final ListIterator<EconomyUserInventoryEntry> iterator = user.getInventory().getEntries().listIterator();
 
-                    while (iterator.hasNext()) {
-                        final EconomyUserInventoryEntry entry = iterator.next();
-                        if (entry.isMature()) {
-                            iterator.remove();
-                            double coins = Forecast.getForecast().getData()[0].getValue() * entry.getCount();
-                            user.addCoins(coins);
-                            modified = true;
+                        while (iterator.hasNext()) {
+                            final EconomyUserInventoryEntry entry = iterator.next();
+                            if (entry.isMature()) {
+                                iterator.remove();
+                                double coins = Forecast.getForecast().getData()[0].getValue() * entry.getCount();
+                                user.addCoins(coins);
+                                modified = true;
+                            }
+                        }
+                        if (modified) {
+                            UserDatabase.updateUser(user);
                         }
                     }
-                    if (modified) {
-                        UserDatabase.updateUser(user);
-                    }
                 }
-            }
-        }, 0, 1, TimeUnit.HOURS);
+            }, 0, 1, TimeUnit.HOURS);
+        }
 
     }
 
@@ -133,7 +104,10 @@ public class Economy implements Plugin {
         commands.add(new GiveCommand());
         commands.add(new ForecastCommand());
         commands.add(new InventoryCommand());
-        commands.add(new CollectCommand());
+        commands.add(new HelpCommand());
+        if (!economyConfig.isAutoCollect())
+            commands.add(new CollectCommand());
+
         return commands;
     }
 
