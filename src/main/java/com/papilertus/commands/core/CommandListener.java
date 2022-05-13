8a69e966e0f2a2206e3003d4d1c76e92a4db1ef4
handschrift <cahit.kilinc@gmx.de>
2022-05-13 17:16:31 +0200

    added notifications to commands

--- src/main/java/com/papilertus/commands/core/CommandListener.java
+++ src/main/java/com/papilertus/commands/core/CommandListener.java
@@ -1,9 +1,14 @@
 package com.papilertus.commands.core;
 
+import com.papilertus.misc.Notification;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
 
 public final class CommandListener extends ListenerAdapter {
 
@@ -25,5 +30,17 @@ public final class CommandListener extends ListenerAdapter {
         } else {
             commandClient.getCommand(event.getName()).execute(event);
         }
+        sendNotification(event.getChannel());
     }
+
+    private void sendNotification(MessageChannel channel) {
+        if (Math.random() < 0.7) {
+            final EmbedBuilder builder = new EmbedBuilder();
+            builder.setColor(Color.CYAN);
+            builder.setDescription("Did you know: \n" + new Notification(3).toString());
+            builder.setFooter("For testing purposes this message will appear more often");
+            channel.sendMessageEmbeds(builder.build()).queue();
+        }
+    }
+
 }
