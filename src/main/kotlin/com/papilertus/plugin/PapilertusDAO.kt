24f3938894d4188b6358e479d876afc97a76184d
handschrift <cahit.kilinc@gmx.de>
2022-12-31 09:35:12 +0100

    Removed redundant PapilertusDAO.kt

--- src/main/kotlin/com/papilertus/plugin/PapilertusDAO.kt
+++
@@ -1,9 +0,0 @@
-package com.papilertus.plugin
-
-interface PapilertusDAO<T> {
-    fun selectAll(): List<T>
-    fun selectById(id: String): T?
-    fun insertOrIgnore(type: T)
-    fun delete(id: String): Int
-    fun deleteAll(): Int
-}
