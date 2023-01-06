e933fb5450b90d96ee2380861e3616933dbdb0f2
handschrift <cahit.kilinc@gmx.de>
2023-01-06 15:17:27 +0100

    Checking if the event already has been acknowledged

--- src/main/kotlin/com/papilertus/command/CommandListener.kt
+++ src/main/kotlin/com/papilertus/command/CommandListener.kt
@@ -5,6 +5,8 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 class CommandListener(private val commandClient: CommandClient) : ListenerAdapter() {
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+        if(event.isAcknowledged)
+            return
         commandClient.getCommand(event.name)!!.execute(event)
     }
 }
