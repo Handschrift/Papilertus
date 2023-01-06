7add9c9d7f6e4ed80eee1bbea3d77b0f82283ac0
handschrift <cahit.kilinc@gmx.de>
2023-01-06 15:17:20 +0100

    Checking if the event already has been acknowledged

--- src/main/kotlin/com/papilertus/gui/modal/ModalListener.kt
+++ src/main/kotlin/com/papilertus/gui/modal/ModalListener.kt
@@ -6,6 +6,8 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 class ModalListener : ListenerAdapter() {
     override fun onModalInteraction(event: ModalInteractionEvent) {
+        if(event.isAcknowledged)
+            return
         val modal = getModalById(event.modalId)
         if (modal == null) {
             event.reply("This modal expired! Please execute the command again.").setEphemeral(true).queue()
