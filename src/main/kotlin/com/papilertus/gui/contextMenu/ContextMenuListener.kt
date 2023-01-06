e85efa92f771bd979461c2678d84b064d1d8ece1
handschrift <cahit.kilinc@gmx.de>
2023-01-06 15:17:24 +0100

    Checking if the event already has been acknowledged

--- src/main/kotlin/com/papilertus/gui/contextMenu/ContextMenuListener.kt
+++ src/main/kotlin/com/papilertus/gui/contextMenu/ContextMenuListener.kt
@@ -6,6 +6,8 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 class ContextMenuListener(private val commandClient: CommandClient) : ListenerAdapter() {
     override fun onUserContextInteraction(event: UserContextInteractionEvent) {
+        if(event.isAcknowledged)
+            return
         commandClient.getContextMenuEntry(event.name)!!.execute(event)
     }
 }
